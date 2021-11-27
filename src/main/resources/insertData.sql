66604a638f2a49eee051eb5b781eb44250bbd456
rafion <rafion.torres@gmail.com>
2021-11-27 11:02:57 -0300

    mais livros

--- src/main/resources/insertData.sql
+++ src/main/resources/insertData.sql
@@ -1,15 +1,20 @@
-INSERT INTO AUTOR (NOME)
+INSERT INTO AUTOR (ID, NOME)
  VALUES 
-('Jose Saramago'),
-('Clarice Lispector'),
-('Edgar Allan Poe'),
-('Fiódor Dostoiévski'),
-('William Shakespeare'),
-('Marcel Proust'),
-('Miguel de Cervantes'),
-('Gabriel García Márquez'),
-('Franz Kafka'),
-('Jorge Luis Borges');
+(1, 'Jose Saramago'),
+(2, 'Clarice Lispector'),
+(3, 'Edgar Allan Poe'),
+(4, 'Fiódor Dostoiévski'),
+(5, 'William Shakespeare'),
+(6, 'Marcel Proust'),
+(7, 'Miguel de Cervantes'),
+(8, 'Gabriel García Márquez'),
+(9, 'Franz Kafka'),
+(10, 'Jorge Luis Borges'),
+(11, 'George R. R. Martin'),
+(12, 'George Orwell'),
+(13, 'Thierry Murat'),
+(14, 'Stephen King');
+
 
 
 INSERT INTO EDITORA (NOME)
@@ -27,12 +32,25 @@ INSERT INTO EDITORA (NOME)
 
 INSERT INTO LIVRO (TITULO, ISBN, AUTOR_ID, EDITORA_ID, PRECO_UNITARIO, DISPONIVEL, URL_IMAGE)
  VALUES
-('Terra do Pecado', '12345678', 1, 1, 300.00, TRUE, 'https://images-na.ssl-images-amazon.com/images/I/41ADIni5wrL.jpg'),
-('Perto do coração selvagem', '12345999', 2, 1, 59.90, TRUE, 'https://m.media-amazon.com/images/P/B07ZH1RNVB.01._SCLZZZZZZZ_SX500_.jpg'),
-('O corvo', '12345644', 3, 1, 29.90, TRUE, 'https://images-na.ssl-images-amazon.com/images/I/515xkV5l3XL._SX331_BO1,204,203,200_.jpg'),
-('O idiota', '1234566', 4, 1, 199.90, TRUE, 'https://m.media-amazon.com/images/P/B08FRJKSM9.01._SCLZZZZZZZ_SX500_.jpg'),
-('irmãos Karamazov', '123454158', 1, 2, 99.90, TRUE, 'https://images-na.ssl-images-amazon.com/images/I/51g-IHo9qCL._SX301_BO1,204,203,200_.jpg'),
-('Hamlet ', '123455158', 5, 3, 79.90, TRUE, 'https://images-na.ssl-images-amazon.com/images/I/51g-IHo9qCL._SX301_BO1,204,203,200_.jpg');
+('Terra do Pecado', '12', 1, 1, 300.00, TRUE, 'https://images-na.ssl-images-amazon.com/images/I/41ADIni5wrL.jpg'),
+('Perto do coração selvagem', '123', 2, 1, 59.90, TRUE, 'https://m.media-amazon.com/images/P/B07ZH1RNVB.01._SCLZZZZZZZ_SX500_.jpg'),
+('O corvo', '1234', 3, 1, 29.90, TRUE, 'https://images-na.ssl-images-amazon.com/images/I/515xkV5l3XL._SX331_BO1,204,203,200_.jpg'),
+('O idiota', '12345', 4, 1, 199.90, TRUE, 'https://m.media-amazon.com/images/P/B08FRJKSM9.01._SCLZZZZZZZ_SX500_.jpg'),
+('irmãos Karamazov', '123456', 1, 2, 99.90, TRUE, 'https://images-na.ssl-images-amazon.com/images/I/51g-IHo9qCL._SX301_BO1,204,203,200_.jpg'),
+('Hamlet ', '1122', 5, 3, 79.90, TRUE, 'https://images-na.ssl-images-amazon.com/images/I/51g-IHo9qCL._SX301_BO1,204,203,200_.jpg'),
+('A Guerra dos Tronos : As Crônicas de Gelo e Fogo','2233', 11, 1 , 59.90 , TRUE, 'https://m.media-amazon.com/images/P/B07PPB9QW1.01._SCLZZZZZZZ_SX500_.jpg'),
+('A fúria dos reis: As Crônicas de Gelo e Fogo','3322', 11, 1 , 49.90 , TRUE, 'https://m.media-amazon.com/images/P/B07QYPLKV1.01._SCLZZZZZZZ_SX500_.jpg'),
+('A tormenta de espadas: As Crônicas de Gelo e Fogo','3344', 11, 1 , 39.90 , TRUE, 'https://m.media-amazon.com/images/P/B07QXSYDSB.01._SCLZZZZZZZ_SX500_.jpg'),
+('O Festim dos Corvos: As Crônicas de Gelo e Fogo','3355', 11, 1 , 59.60 , TRUE, 'https://m.media-amazon.com/images/P/B081284MLJ.01._SCLZZZZZZZ_SX500_.jpg'),
+('A dança dos dragões: As Crônicas de Gelo e Fogo','3366', 11, 1 , 48.62 , TRUE, 'https://m.media-amazon.com/images/P/B084SS25D3.01._SCLZZZZZZZ_SX500_.jpg'),
+('Fogo & Sangue','3377', 11, 1 , 36.60 , TRUE, 'https://m.media-amazon.com/images/P/B07HKPLNHC.01._SCLZZZZZZZ_SX500_.jpg'),
+('A revolução dos bichos: Um conto de fadas','3388', 12, 1 , 14.90 , TRUE, 'https://images-na.ssl-images-amazon.com/images/I/61owA5ey3iL._SX324_BO1,204,203,200_.jpg'),
+('1984','3323', 11, 1 , 8.90 , TRUE, 'https://m.media-amazon.com/images/P/B08X1WDMQ2.01._SCLZZZZZZZ_SX500_.jpg'),
+('O velho e o mar','1132', 13, 1 , 34.80 , TRUE, 'https://m.media-amazon.com/images/P/B0774WN7QF.01._SCLZZZZZZZ_SX500_.jpg'),
+('O cemitério','1136', 14, 1 , 34.90 , TRUE, 'https://m.media-amazon.com/images/P/B00CEZUV26.01._SCLZZZZZZZ_SX500_.jpg'),
+('It: A coisa','1131', 14, 1 , 39.90 , TRUE, 'https://m.media-amazon.com/images/P/B00MEQSA4Q.01._SCLZZZZZZZ_SX500_.jpg'),
+('O iluminado','1139', 14, 1 , 59.90 , TRUE, 'https://m.media-amazon.com/images/P/B0743MBY15.01._SCLZZZZZZZ_SX500_.jpg'),
+('The Stand','1137', 14, 1 , 59.90 , TRUE, 'https://m.media-amazon.com/images/P/B001C4NXKM.01._SCLZZZZZZZ_SX500_.jpg');
 
 INSERT INTO MEIO_PAGAMENTO (NOME)
 VALUES

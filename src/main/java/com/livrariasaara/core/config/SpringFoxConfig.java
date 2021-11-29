50cf6072c6caa06980208db5a4aa6f07b974c0a6
rafion <rafion.torres@gmail.com>
2021-11-29 11:58:00 -0300

    add swagger basic documentation

---
+++ src/main/java/com/livrariasaara/core/config/SpringFoxConfig.java
@@ -0,0 +1,36 @@
+package com.livrariasaara.core.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SpringFoxConfig {
+
+	 @Bean
+	    public Docket api() { 
+	        return new Docket(DocumentationType.SWAGGER_2)  
+	          .select()                                  
+	          .apis(RequestHandlerSelectors.any())              
+	          .paths(PathSelectors.any())                          
+	          .build();                                           
+	    }
+	 
+	 private ApiInfo apiInfo() {
+		    return new ApiInfo(
+		      "My REST API", 
+		      "Some custom description of API.", 
+		      "API TOS", 
+		      "Terms of service", 
+		      new Contact("Ráfion", "https://github.com/rafion", "rafion.torres@gmail.com"), 
+		      "License of API", "API license URL", Collections.emptyList());
+		}
+}
